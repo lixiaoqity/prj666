0559b34686e9ffc19f55f5ea96587d63e770444f
Roger Yu <roger.yu27@gmail.com>
2021-01-25 21:37:00 -0500

    Removed global mysql

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const mysql = require("mysql2/promise");
 const cookieParser = require("cookie-parser");
 const auth = require("./routes/authRoutes");
 
@@ -12,15 +11,6 @@ const server = require("http").createServer(app);
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
-
-const connect = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-});
-
-global.connect = connect;
 
 app.use("/api/resetpassword", auth);
 
